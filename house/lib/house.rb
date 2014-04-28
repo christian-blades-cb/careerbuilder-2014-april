ce29fd6369b1fded4cffc2b21357f42f0a30f476
Christian Blades <christian.blades@careerbuilder.com>
2014-04-28 12:47:17 -0400

    Yup

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,51 +1,28 @@
 class House
-  def initialize()
-    
-  @bleh = [
-      "This is the house that Jack built.",
-      "This is the malt that lay in the house that Jack built.",
-      "This is the rat that ate the malt that lay in the house that Jack built.",
-      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
-      "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+
+  def things
+    [
+      "horse and the hound and the horn that belonged to",
+      "farmer sowing his corn that kept",
+      "rooster that crowed in the morn that woke",
+      "priest all shaven and shorn that married",
+      "man all tattered and torn that kissed",
+      "maiden all forlorn that milked",
+      "cow with the crumpled horn that tossed",
+      "dog that worried",
+      "cat that killed",
+      "rat that ate",
+      "malt that lay in",
+      "house that Jack built"
     ]
   end
   
   def line(num)
-    @bleh[num-1]+"\n"
+    nested_things = things.last(num).map {|x| "the #{x}"}.join(" ")
+    "This is #{nested_things}.\n"
   end
 
   def recite
-    <<-TEXT
-This is the house that Jack built.
-
-This is the malt that lay in the house that Jack built.
-
-This is the rat that ate the malt that lay in the house that Jack built.
-
-This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-
-This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-    TEXT
+    1.upto(things.count).map {|x| line(x)}.join("\n")
   end
 end
