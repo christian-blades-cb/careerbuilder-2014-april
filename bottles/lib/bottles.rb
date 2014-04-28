33f4c1e58b669a507c47829a88063af61e5ba742
Christian Blades <christian.blades@careerbuilder.com>
2014-04-28 14:56:55 -0400

    rename

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,22 +4,22 @@ class Bottles
     when 0
       <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, #{pluraled_bottle(99)} of beer on the wall.
+Go to the store and buy some more, #{plural_bottle(99)} of beer on the wall.
 VERSE
     when 1
       <<-VERSE
-#{pluraled_bottle(num)} of beer on the wall, #{pluraled_bottle(num)} of beer.
+#{plural_bottle(num)} of beer on the wall, #{plural_bottle(num)} of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 VERSE
     else
       <<-VERSE
-#{pluraled_bottle(num)} of beer on the wall, #{pluraled_bottle(num)} of beer.
-Take one down and pass it around, #{pluraled_bottle(num - 1)} of beer on the wall.
+#{plural_bottle(num)} of beer on the wall, #{plural_bottle(num)} of beer.
+Take one down and pass it around, #{plural_bottle(num - 1)} of beer on the wall.
 VERSE
     end
   end
 
-  def pluraled_bottle(num)
+  def plural_bottle(num)
     num > 1 ? "#{num} bottles" : "#{num} bottle"
   end
 
